52cd9d7fe51a558a1890c49652aca0a2b046a9be
seantyh <seantyh@gmail.com>
2019-03-12 22:45:04 +0800

    add steps and change merging algorithm

--- CwnGraph/merger/annot_merger.py
+++ CwnGraph/merger/annot_merger.py
@@ -1,4 +1,5 @@
 from typing import Set, cast
+from itertools import chain
 from ..cwn_types import GraphStructure, CwnNode, CwnSense, CwnSynset
 from ..cwn_types import CwnRelation
 from .merged_annot import MergedAnnotation
@@ -26,7 +27,8 @@ class AnnotationMerger:
 
         meta_data = {
             "trace": self.trace,
-            "conflicts": self.conflicts
+            "conflicts": self.conflicts,
+            "steps": self.steps
         }
         Gm = MergedAnnotation(meta_data, self.Vm, self.Em)
 
@@ -36,74 +38,125 @@ class AnnotationMerger:
         self.serial += 1
         return f"{self.hashStr}_{self.serial}"
     
+    def map_new_edge_id(self, edge_id):
+        from_id, to_id = edge_id
+        if from_id not in self.merged_idmap:
+            raise ValueError(f"{from_id} not found when merging")
+
+        if to_id not in self.merged_idmap:
+            raise ValueError(f"{to_id} not found when merging")
+        
+        return (self.merged_idmap[from_id], 
+                self.merged_idmap[to_id])
+
     def merge_nodes(self, Vx, Vy):                
-        nodes_base = {CwnNodeFactory.createNode(y, self.y.cgu) for y in Vy}
-        nodes_merged = set()
-        for node_id_x in Vx.keys():
-            node_x: CwnNode = CwnNodeFactory.createNode(node_id_x, self.x.cgu)
-            
-            if node_x in nodes_base:
-                # there are two identitcal node in Vx and Vy
-                node_x = self.merge_node(node_x, nodes_base)
-                if node_x.node_type == "sense":                    
-                    sense_x: CwnSense = self.merge_sense_node(node_x, nodes_base)
-                    nodes_merged.add(sense_x)
-                elif node_x.node_type == "synset":
-                    synset_x: CwnSynset = self.merge_synset_node(node_x, nodes_base)
-                    nodes_merged.add(synset_x)
-                else:
-                    nodes_merged.add(node_x)
-            else:
+        
+        nodes_map = {}
+        # index nodes in Vx
+        for node_x in Vx:
+            node_obj = CwnNodeFactory.createNode(node_x, self.x.cgu)            
+            nodes_map.setdefault(node_obj, []).append(node_obj)
+
+        # index nodes in Vy
+        for node_y in Vy:
+            node_obj = CwnNodeFactory.createNode(node_y, self.y.cgu)            
+            nodes_map.setdefault(node_obj, []).append(node_obj)
+        
+        nodes_merged = {}
+        for nodes_list in nodes_map.values():
+            new_id = self.new_node_id(node_x.node_type)
+
+            if len(nodes_list) > 1:
                 # node_x is unique in Vx, add it to nodes_base
-                nodes_merged.add(node_x)
-            
+                node_x = nodes_list
+                nodes_merged[new_id] = node_x
+                self.merged_idmap[node_x.id] = new_id
+                self.steps.append(("unique", new_id, node_x.id))
+
+            elif len(nodes_list) > 1:
+                # there are two identitcal node in Vx and Vy
+                node_x, node_y = nodes_list[0], nodes_list[1]
+                node_m = self.merge_node(node_x, node_y)
+                if node_x.node_type == "sense":                    
+                    sense_x: CwnSense = self.merge_sense_node(node_x, node_y)
+                    nodes_merged[new_id] = sense_x
+                elif node_x.node_type == "synset":
+                    synset_x: CwnSynset = self.merge_synset_node(node_x, node_y)
+                    nodes_merged[new_id] = synset_x
+                else:
+                    nodes_merged[new_id] = node_x
+                self.steps.append(("merged", new_id, f"{node_x.id},{node_y.id}"))
+            else:
+                print("ERROR: " + str(nodes_list))
+                raise ValueError("More than two nodes are identical")
+                            
         # add all nodes into merged Graph nodes, Vm
-        for node_m in nodes_merged:            
-            self.Vm[node_m.id] = node_m.data()
+        for node_id, node_m in nodes_merged.items():            
+            self.Vm[node_id] = node_m.data()            
 
     def merge_node(self, 
-        node_x: CwnNode, nodes_set: Set[CwnNode]) -> CwnNode:
-        for node_y in nodes_set:
-            if node_y != node_x: continue                        
-            node_x.annot = {
+        node_x: CwnNode, node_y: CwnNode) -> CwnNode:
+        if node_y != node_x: return node_x
+        
+        node_x.annot = {
                 self.x.name: node_x.annot,
                 self.y.name: node_y.annot
             }
         return node_x
 
     def merge_sense_node(self, 
-        sense_x: CwnSense, nodes_set: Set[CwnNode]) -> CwnSense:
-        for node_y in nodes_set:
-            if node_y != sense_x: continue
-            sense_y: CwnSense = cast(CwnSense, node_y)
-            for ex_y in sense_y.examples:
-                if ex_y not in sense_x.examples:
-                    sense_x.examples.append(ex_y)
-            sense_x.domain = ",".join([sense_x.domain, sense_y.domain])
+        sense_x: CwnSense, sense_y: CwnSense) -> CwnSense:        
+        if sense_x != sense_y:
+            return sense_x
+
+        for ex_y in sense_y.examples:
+            if ex_y not in sense_x.examples:
+                sense_x.examples.append(ex_y)
+        sense_x.domain = ",".join([sense_x.domain, sense_y.domain])
         return sense_x
 
     def merge_synset_node(self, 
-        synset_x: CwnSynset, nodes_set: Set[CwnNode]) -> CwnSynset:
-        for node_y in nodes_set:
-            if node_y != synset_x: continue
-            synset_y: CwnSynset = cast(CwnSynset, node_y)
-            synset_x.pwn_id = ",".join([synset_x.pwn_id, synset_y.pwn_id])
-            synset_x.pwn_word = ",".join([synset_x.pwn_word, synset_y.pwn_word])
+        synset_x: CwnSynset, synset_y: CwnSynset) -> CwnSynset:        
+        if synset_x != synset_y:
+            return synset_x
+        
+        synset_x.pwn_id = ",".join([synset_x.pwn_id, synset_y.pwn_id])
+        synset_x.pwn_word = ",".join([synset_x.pwn_word, synset_y.pwn_word])
         return synset_x
 
     def merge_edges(self, Ex, Ey):
-        edges_base = {CwnEdgeFactory.createEdge(y, self.y.cgu) for y in Ey}
-        edges_merged = set()
-        for edge_id_x in Ex.keys():
-            edge_x: CwnRelation = CwnEdgeFactory.createEdge(edge_id_x, self.x.cgu)
-            
-            if edge_x in edges_base:
-                # there are two identitcal edge in Vx and Vy                
-                edge_x = self.merge_edge(edge_x, edges_base)
-                edges_merged.add(edge_x)
+        edges_map = {}
+        # index nodes in Vx
+        for edge_x in Ex:
+            edge_obj = CwnEdgeFactory.createEdge(edge_x, self.x.cgu)            
+            edges_map.setdefault(edge_obj, []).append(edge_obj)
+
+        # index nodes in Vy
+        for edge_y in Ey:
+            edge_obj = CwnEdgeFactory.createEdge(edge_y, self.y.cgu)            
+            edges_map.setdefault(edge_obj, []).append(edge_obj)
+        
+        edges_merged = {}        
+        for edges_list in edges_merged.values():                        
+            if len(edges_list) == 1:
+                edge_x = edges_list[0]
+                new_edge_id = self.map_new_edge_id(edge_x.id)
+                edges_merged[new_edge_id] = edge_x
+                self.steps.append(("unique", new_edge_id, f"{edge_x.id}"))
+            elif len(edges_list) == 2:
+                edge_x, edge_y = edges_list
+                try:
+                    edge_x = self.merge_edge(edge_x, edge_y)
+                    new_edge_id = self.map_new_edge_id(edge_x.id)
+                    edges_merged[new_edge_id] = edge_x
+                    self.steps.append(("merged", new_edge_id, f"{edge_x.id},{edge_y.id}"))
+                except ValueError:
+                    self.add_conflict_entry(edge_x, edge_y)                               
+                    self.steps.append(("conflict", new_edge_id, f"{edge_x.id},{edge_y.id}"))                 
             else:
-                # edge_x is unique in Vx, add it to edges_base
-                edges_merged.add(edge_x)
+                print("ERROR: " + str(edges_list))
+                raise ValueError("More than two edges are identical")
+                
             
         # add all edges into merged Graph edges, Vm
         for edge_m in edges_merged:
@@ -114,7 +167,7 @@ class AnnotationMerger:
         for edge_y in edges_set:
             if edge_y != edge_x: continue    
             if edge_y.edge_type != edge_x.edge_type:
-                self.add_conflict_entry(edge_x, edge_y)
+                raise ValueError("edge_type mismatch")                
             edge_x.annot = {
                 self.x.name: edge_x.annot,
                 self.y.name: edge_y.annot
